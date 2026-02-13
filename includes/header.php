3fd5c49446f6f7a4901133037cab7606c044cfb6
Lam <chrood.pro@proton.me>
2026-02-13 15:23:05 +0100

    base index body

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Site PHP</title>
+    <link rel="stylesheet" href="assets/css/script.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php?page=home">Accueil</a></li>
+                <li><a href="index.php?page=login">Connexion</a></li>
+                </ul>
+        </nav>
+    </header>
+    <main></main>
