acc559951ed207802df61eb26e22cdfe52ecc09a
Accoow <54588330+Accoows@users.noreply.github.com>
2026-02-16 16:19:36 +0100

    feat: add .env for database setup

--- config/database.php
+++ config/database.php
@@ -1,9 +1,17 @@
 <?php
-$host = '127.0.0.1';
-$port = '3307';
-$dbname = 'php_exam_db';
-$username = 'root';
-$password = '';
+
+$envPath = __DIR__ . '/../.env';
+
+if (!file_exists($envPath)) {
+    die("Le fichier .env est introuvable");
+}
+$env = parse_ini_file($envPath);
+
+$host = $env['DB_HOST'];
+$port = $env['DB_PORT'];
+$dbname = $env['DB_NAME'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
